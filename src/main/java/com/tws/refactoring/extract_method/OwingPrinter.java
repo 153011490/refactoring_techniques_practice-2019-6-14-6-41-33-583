8e8064993a7b2f520d5ea74b4a51319de805981a
SamsonLin <153011490@qq.com>
2019-07-15 17:52:34 +0800

    Refactor:extract calculate process to be a method named calculateAmount

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -1,24 +1,27 @@
 package com.tws.refactoring.extract_method;
 
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 public class OwingPrinter {
-    void printOwing(String name, List<Order> orders) {
+   public void printOwing(String name, List<Order> orders) {
         Iterator<Order> elements = orders.iterator();
         double outstanding = 0.0;
-
         // print owings
-        while (elements.hasNext()) {
-            Order each = (Order) elements.next();
-            outstanding += each.getAmount();
-        }
+       outstanding=calculateAmount(elements);
+
         printBanner();
         // print details
         System.out.println("name: " + name);
         System.out.println("amount: " + outstanding);
+    }
+
+    public double calculateAmount(Iterator<Order> elements){
+        double outstanding = 0.0;
+        while (elements.hasNext()) {
+            Order each = (Order) elements.next();
+            outstanding += each.getAmount();
+        }
+        return outstanding;
     }
 
     public void printBanner(){
