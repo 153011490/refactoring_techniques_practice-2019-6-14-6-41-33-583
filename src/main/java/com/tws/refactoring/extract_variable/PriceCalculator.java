261ca36b61218020319349fa78b653fc8c146619
SamsonLin <153011490@qq.com>
2019-07-15 18:58:06 +0800

    Refactor:extract magic numbers to be constants

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,10 +1,18 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
+
+    private static final int OFF_QUANTITY= 500;
+    private static final double OFF_DISCOUNT = 0.05;
+    private static final double SHIPPING_COST_DISCOUNT = 0.1;
+    private static final double MAX_SHIPPING_COST = 100;
+
+
     double getPrice(int quantity, int itemPrice) {
         // Price consists of: base price - discount + shipping cost
         return quantity * itemPrice -
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+                Math.max(0, quantity - OFF_QUANTITY) * itemPrice * OFF_DISCOUNT +
+                Math.min(quantity * itemPrice * SHIPPING_COST_DISCOUNT, MAX_SHIPPING_COST);
+
     }
 }
