6afe5b36b624030ff6535facac79a51a1a9706e5
SamsonLin <153011490@qq.com>
2019-07-15 16:47:24 +0800

    Test:add test for OwingPrinter print Owing

--- src/test/java/com/tws/refactoring/extract_method/OwingPrinterTest.java
+++ src/test/java/com/tws/refactoring/extract_method/OwingPrinterTest.java
@@ -1,11 +1,15 @@
 package com.tws.refactoring.extract_method;
 
+
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -27,5 +31,18 @@ public class OwingPrinterTest {
     public void printOwing() {
 //        System.out.print("hello");
 //        assertEquals("hello", outContent.toString());
+
+        //given
+        OwingPrinter owingPrinter=new OwingPrinter();
+        String name="Mike";
+        List<Order> orders=new ArrayList<>();
+        orders.add(new Order(20.0));
+
+        //when
+        owingPrinter.printOwing(name,orders);
+        String expected="*****************************\r\n"+"****** Customer totals ******\r\n"+"*****************************\r\n"+"name: Mike\r\n"+"amount: 20.0\r\n";
+
+        //then
+        assertEquals(expected,outContent.toString());
     }
 }
